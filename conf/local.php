639adc9ace04ee8c86640e6d7c2c9350e7b6a439
Nico <nico@tabulify.com>
2023-04-18 20:58:56 +0000

    Beau modifications

--- conf/local.php
+++ conf/local.php
@@ -7,14 +7,10 @@
  */
 
 $conf['title'] = 'Tabulify';
-$conf['license'] = 'cc-by-sa';
 $conf['useheading'] = '1';
 $conf['useacl'] = 1;
 $conf['superuser'] = '@admin';
 $conf['disableactions'] = 'register';
-$conf['renderer_xhtml'] = 'combo_renderer';
 $conf['plugin']['combo']['mandatoryQualityRules'] = 'internal_backlinks_min,internal_links_min,words_min';
 $conf['plugin']['combo']['outlineNumberingEnable'] = 1;
-$conf['tpl']['strap']['footerSlotPageName'] = 'slot_footer';
-$conf['tpl']['strap']['headerSlotPageName'] = 'slot_header';
-$conf['tpl']['strap']['sidekickSlotPageName'] = 'slot_sidekick';
+$conf['plugin']['combo']['primaryColor'] = '#326e32`;
